9756c3926158fc321f546c0930d8e8b2069d217d
Nayara Dornelas <nayaradornelas@id.uff.br>
2023-03-07 17:00:03 -0300

    feat(ajuste de codigos)

---
+++ themes/ExpandJr/functions.php
@@ -0,0 +1,40 @@
+<?php 
+// Variables
+define('ROOT_DIR', get_theme_file_path());
+define('STYLES_DIR', get_template_directory_uri() . '/assets/css');
+define('IMAGES_DIR', get_template_directory_uri() . '/assets/images');
+define('JS_DIR', get_template_directory_uri() . '/assets/js');
+define('INC_DIR', ROOT_DIR . '/includes');
+define('INCLUDES_DIR', ROOT_DIR . '/includes');
+define('SCRIPTS_DIR', get_template_directory_uri() . '/assets/js');
+
+
+// Includes
+include_once(INCLUDES_DIR . '/enqueue.php');
+include_once(INCLUDES_DIR . '/setup-theme.php');
+
+
+// Hooks
+add_action('wp_enqueue_scripts', 'expandjr_style');
+add_action('after_setup_theme', 'expandjr_setup_theme');
+add_action('wp_head', 'wpb_track_post_views');
+
+// Functions
+
+/* Incrementa as views de um post quando acessado */
+function wpb_track_post_views($post_id) {
+    if ( !is_single() ) return;
+    if ( empty ( $post_id) ) {
+        global $post;
+        $post_id = $post->ID;    
+    }
+    $views = get_post_meta( $post_id, 'post_views_count', true );
+    if ( !$views ) {
+        $views = 0;
+    }
+    $views++;
+    update_post_meta( $post_id, 'post_views_count', $views );
+}
+
+
+?> 
--- themes/ExpandJr/inc/enqueue.php
+++ themes/ExpandJr/inc/enqueue.php
@@ -1,22 +1,40 @@
 <?php
 
-function expandjr_enqueue_style(){
+function expandjr_style(){
     
-    // Register style
-    wp_register_style('expandjr-reset', STYLE_DIR . '/reset.css', [], '1.0.0', false);
-    wp_register_style('expandjr-style', STYLE_DIR . '/style.css', [], '1.0.0', false);
-    wp_register_style('expandjr-footer', STYLE_DIR . '/footer.css', [], '1.0.0', false);
+    // Register 
+    /* style */
+    wp_register_style('expandjr-reset', STYLES_DIR . '/reset.css', [], '1.0.0', false);
+    wp_register_style('expandjr-style', STYLES_DIR . '/style.css', [], '1.0.0', false);
+    wp_register_style('expandjr-footer', STYLES_DIR . '/footer.css', [], '1.0.0', false);
+    wp_register_style('expandjr-blog', STYLES_DIR . '/page-blog.css', [], '1.0.0', false);
+    wp_register_style('expandjr_header', STYLES_DIR . '/header.css', [], '1.0.0', false);
+    wp_register_style('expandjr-home-style', STYLES_DIR . '/home.css', [], '1.0.0', false);
+    wp_register_style('expandjr-slider1-style', STYLES_DIR . '/slider1.css', [], '1.0.0', false);
+    
     /* fonts */
     wp_register_style('expandjr-barlow-font', 'https://fonts.googleapis.com/css2?family=Barlow:wght@500;600;700&display=swap');
     wp_register_style('expandjr-roboto-font', 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap');
 
-    // Enqueue style
+    
+    /* js */
+    wp_register_script('slider-cases', SCRIPTS_DIR .'/slider.js', [], '1.0.0', true);
+    wp_register_script('header_script', JS_DIR . '/header.js', [], '1.0.0', true);
+
+    // Enqueue 
+    /* style */
     wp_enqueue_style('expandjr-reset');
     wp_enqueue_style('expandjr-style');
-    wp_enqueue_style('expandjr-footer');
+    wp_enqueue_style('expandjr-home-style');
+    wp_enqueue_style('expandjr-blog');
+    wp_enqueue_style('expandjr_header');
+    wp_enqueue_style('expandjr-slider1-style');
     /* fonts */
     wp_enqueue_style('expandjr-barlow-font');
     wp_enqueue_style('expandjr-roboto-font');
-}
 
-?>
+    wp_enqueue_style('google-fonts','https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;700&display=swap', [], null);
+    /* js */
+    wp_enqueue_script('header_script');
+
+}

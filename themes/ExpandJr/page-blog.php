5fda015fe00960838455cf495a24db4089b35eef
NathanMad <nathan@internit.com.br>
2023-03-12 00:57:48 -0300

    feat: adicionando filtro por categoria

--- themes/ExpandJr/page-blog.php
+++ themes/ExpandJr/page-blog.php
@@ -14,11 +14,32 @@
     <div>
 
         <?php
-
         $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+        if (isset($_GET)) {
+            $categories = '';
+            foreach ($_GET as $key => $value) {
+                if (strpos($key, 'categoria') !== false) {
+                    $categories = $categories . $value . ',';
+                };
+            }
 
-        $allPostsWPQuery = new WP_Query(array('post_type' => 'post', 'post_status' => 'publish', 'posts_per_page' => 3, 'paged' => $paged));
-        if ($allPostsWPQuery->have_posts()) : while ($allPostsWPQuery->have_posts()) : $allPostsWPQuery->the_post();
+            $categories = new WP_Query(array(
+                'post_type' => 'post', 
+                'post_status' => 'publish', 
+                'posts_per_page' => 3, 
+                'paged' => $paged,
+                'category_name' => substr($categories, 0, strlen($categories) - 1)
+            ));
+            echo '<pre>';
+            print_r($categories->posts);
+            echo '</pre>';
+        } else {
+            $categories = new WP_Query(array('post_type' => 'post', 'post_status' => 'publish', 'posts_per_page' => 3, 'paged' => $paged));
+        };
+
+        if ($categories->have_posts()) {
+            while ($categories->have_posts()) {
+                $categories->the_post();
         ?>
                 <div class='blog-card'>
 
@@ -34,19 +55,30 @@
                     </div>
                 </div>
                 <br>
-            <?php endwhile; ?>
+            <?php }; ?>
             <div class='pagination'>
 
                 <?= paginate_links(array(
-                    'total' => $allPostsWPQuery->max_num_pages
+                    'total' => $categories->max_num_pages
                 )); ?>
 
             </div>
     </div>
-    <div></div>
+    <form action="" method='get' name='busca_blog' id='busca_blog' style='display:flex;flex-direction:column;gap:20px'>
+        <input type="text" name='busca_titulo' class='busca' placeholder='busca' style='border:1px solid black;padding:5px'>
+        <ul style='display:flex;flex-direction:column;gap:10px;align-items:start'>
+            <?php
+            $counter = 0;
+            foreach (get_categories() as $x) {
+                echo "<li>" . $x->name . " <input type='checkbox' value='" . $x->slug . "' name='categoria" . ++$counter . "'></li>";
+            }
+            ?>
+        </ul>
+        <input type="submit" value="BUSCAR" style="padding: 10px;background:black;color:white;width:min-content">
+    </form>
 </section>
 
-<?php endif; ?>
+<?php  }; ?>
 <?php wp_reset_query(); ?>
 
 <?php get_footer(); ?>
